fc75a0ab531859911c7a396b65d6fac00d14d755
Matej Galić <matej.galic.ets@gmail.com>
2024-06-06 12:52:25 +0200

    conan build

--- CMakeProjects/IntroLibTest/IntroLibTest.cmake
+++ CMakeProjects/IntroLibTest/IntroLibTest.cmake
@@ -1,10 +1,3 @@
-include(FetchContent)
-FetchContent_Declare(
-  googletest
-  URL https://github.com/google/googletest/archive/release-1.11.0.zip
-)
-FetchContent_MakeAvailable(googletest)
-
 include(${CMAKE_CURRENT_LIST_DIR}/IntroLibTest.srcs.cmake)
 
 add_executable(IntroLibTests ${TEST_SOURCES})
