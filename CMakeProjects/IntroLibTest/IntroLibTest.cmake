413e2e0d4b92a98c0a96dc31776b01433ddd96bb
Matej Galić <matej.galic.ets@gmail.com>
2024-06-06 13:15:37 +0200

    gtest error

--- CMakeProjects/IntroLibTest/IntroLibTest.cmake
+++ CMakeProjects/IntroLibTest/IntroLibTest.cmake
@@ -1,7 +1,9 @@
 include(${CMAKE_CURRENT_LIST_DIR}/IntroLibTest.srcs.cmake)
 
+find_package(GTest REQUIRED)
+
 add_executable(IntroLibTests ${TEST_SOURCES})
-target_link_libraries(IntroLibTests gtest_main IntroLib)
+target_link_libraries(IntroLibTests gtest::gtest IntroLib)
 
 enable_testing()
 include( GoogleTest )
