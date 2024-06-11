76da84f7404e18aad68d3a058dd35c1a1eb42e70
Matej Galić <matej.galic.ets@gmail.com>
2024-06-11 10:11:37 +0200

    fixed building project

--- nn-playground/PlaygroundLib/Source/ImageLoader.cpp
+++ nn-playground/PlaygroundLib/Source/ImageLoader.cpp
@@ -13,59 +13,6 @@ ImageLoader::ImageLoader( const std::string & path ) :
     checkProperties();
 }
 
-void ImageLoader::load( const std::string & path )
-{
-    std::ifstream file( path, std::ios::binary );
-    if ( !file )
-    {
-        throw std::runtime_error( "Failed to open file: " + path );
-    }
-
-    spng_set_png_file( ctx, file.rdbuf() );
-
-    spng_ihdr ihdr;
-    if ( spng_get_ihdr( ctx, &ihdr ) )
-    {
-        throw std::runtime_error( "Failed to get image header" );
-    }
-
-    width  = ihdr.width;
-    height = ihdr.height;
-
-    size_t image_size;
-    if ( spng_decoded_image_size( ctx, SPNG_FMT_RGBA8, &image_size ) )
-    {
-        throw std::runtime_error( "Failed to get decoded image size" );
-    }
-
-    image_data = ( unsigned char * ) malloc( image_size );
-    if ( !image_data )
-    {
-        throw std::runtime_error( "Failed to alocate memory for image" );
-    }
-
-    if ( spng_decode_image( ctx, image_data, image_size, SPNG_FMT_RGBA8, 0 ) )
-    {
-        throw std::runtime_error( "Failed to decode image" );
-    }
-}
-
-void ImageLoader::checkProperties()
-{
-    if ( width != 28 || height != 28 )
-    {
-        throw std::runtime_error( "Image is not 28x28 pixels" );
-    }
-
-    for ( int i = 0; i < width * height * 4; i += 4 )
-    {
-        if ( image_data[ i ] != image_data[ i + 1 ] || image_data[ i ] != image_data[ i + 2 ] )
-        {
-            throw std::runtime_error( "Image is not black and white" );
-        }
-    }
-}
-
 ImageLoader::~ImageLoader()
 {
     if ( ctx )
@@ -91,4 +38,63 @@ int ImageLoader::getWidth() const
 int ImageLoader::getHeight() const
 {
     return height;
+}
+
+void ImageLoader::load( const std::string & path )
+{
+    FILE * file = fopen( path.c_str(), "rb" );
+    if ( !file )
+    {
+        throw std::runtime_error( "Failed to open file: " + path );
+    }
+
+    spng_set_png_file( ctx, file );
+
+    spng_ihdr ihdr;
+    if ( spng_get_ihdr( ctx, &ihdr ) )
+    {
+        fclose( file );
+        throw std::runtime_error( "Failed to get image header" );
+    }
+
+    width  = ihdr.width;
+    height = ihdr.height;
+
+    size_t image_size;
+    if ( spng_decoded_image_size( ctx, SPNG_FMT_RGBA8, &image_size ) )
+    {
+        fclose( file );
+        throw std::runtime_error( "Failed to get decoded image size" );
+    }
+
+    image_data = ( unsigned char * ) malloc( image_size );
+    if ( !image_data )
+    {
+        fclose( file );
+        throw std::runtime_error( "Failed to allocate memory for image" );
+    }
+
+    if ( spng_decode_image( ctx, image_data, image_size, SPNG_FMT_RGBA8, 0 ) )
+    {
+        fclose( file );
+        throw std::runtime_error( "Failed to decode image" );
+    }
+
+    fclose( file );
+}
+
+void ImageLoader::checkProperties()
+{
+    if ( width != 28 || height != 28 )
+    {
+        throw std::runtime_error( "Image is not 28x28 pixels" );
+    }
+
+    for ( int i = 0; i < width * height * 4; i += 4 )
+    {
+        if ( image_data[ i ] != image_data[ i + 1 ] || image_data[ i ] != image_data[ i + 2 ] )
+        {
+            throw std::runtime_error( "Image is not black and white" );
+        }
+    }
 }
--- nn-playground/PlaygroundLibMain/PlaygroundLibMain.cmake
+++ nn-playground/PlaygroundLibMain/PlaygroundLibMain.cmake
@@ -3,6 +3,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/PlaygroundLibMain.srcs.cmake)
 find_package(SPNG REQUIRED)
 
 add_executable(PlaygroundLibMain ${SOURCES})
-target_link_libraries(PlaygroundLibMain spng::spng_static PlaygroundLib)
+
+target_link_libraries(PlaygroundLibMain PRIVATE PlaygroundLib)
 
 enable_testing()
